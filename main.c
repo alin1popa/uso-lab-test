a21ba61a04d995e9ffe26ccde457d946606fdcc8
Alin Popa <alin.popa@softwire.com>
2020-11-10 18:40:57 +0200

    Commit pe branch nou

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 int main()
 {
 	int x, y;
-	printf("Salutare 2! Radical din 25 este %f\n", sqrt(25));
+	printf("Salutare 2! Suntem pe branch nou. Radical din 25 
+este %f\n", sqrt(25));
 	return 0;
 }
